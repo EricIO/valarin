654ee2dde32e9424350256dc23305b33558b23bd
Eric Skoglund <eric@pagefault.se>
2015-05-26 23:17:07 +0200

    Added comments and ignore to grammar

---
+++ src/org/valarin/nodes/ValBinaryNode.java
@@ -0,0 +1,7 @@
+package org.valarin.nodes;
+
+/**
+ * Created by eric on 5/26/15.
+ */
+public class ValBinaryNode {
+}
---
+++ src/org/valarin/nodes/expression/ValAddNode.java
@@ -0,0 +1,7 @@
+package org.valarin.nodes.expression;
+
+/**
+ * Created by eric on 5/26/15.
+ */
+public class ValAddNode {
+}
