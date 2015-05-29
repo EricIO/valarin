830433337aae3c0938e35985105e0718a8f5c980
Eric Skoglund <eric@pagefault.se>
2015-05-29 11:34:40 +0200

    Added more literal

--- src/org/valarin/nodes/expression/ValBigIntegerLiteral.java
+++ src/org/valarin/nodes/expression/ValBigIntegerLiteral.java
@@ -1,7 +1,25 @@
 package org.valarin.nodes.expression;
 
-/**
- * Created by eric on 5/27/15.
- */
-public class ValBigIntegerLiteral {
+import com.oracle.graal.nodeinfo.NodeInfo;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.UnexpectedResultException;
+import org.valarin.nodes.ValExpressionNode;
+
+import java.math.BigInteger;
+
+@NodeInfo(shortName = "const")
+public class ValBigIntegerLiteral extends ValExpressionNode {
+    
+    public final BigInteger value;
+    
+    public ValBigIntegerLiteral(BigInteger big) { this.value = big; }
+    
+    @Override public BigInteger executeBigInteger(VirtualFrame frame) throws UnexpectedResultException {
+        return this.value; 
+    }
+    
+    @Override
+    public Object executeGeneric(VirtualFrame frame) {
+        return this.value;
+    }
 }
--- src/org/valarin/nodes/expression/ValLongLiteralNode.java
+++ src/org/valarin/nodes/expression/ValLongLiteralNode.java
@@ -1,7 +1,23 @@
 package org.valarin.nodes.expression;
 
-/**
- * Created by eric on 5/27/15.
- */
-public class ValLongLiteralNode {
+import com.oracle.graal.nodeinfo.NodeInfo;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.UnexpectedResultException;
+import org.valarin.nodes.ValExpressionNode;
+
+import java.rmi.UnexpectedException;
+import java.util.Objects;
+
+@NodeInfo(shortName = "const")
+public class ValLongLiteralNode extends ValExpressionNode {
+    
+    private final long value;
+    
+    public ValLongLiteralNode(long value) {
+        this.value = value;
+    }
+    
+    @Override public long executeLong(VirtualFrame frame) throws UnexpectedResultException { return this.value; }
+    
+    @Override public Object executeGeneric(VirtualFrame frame) { return value; }
 }
