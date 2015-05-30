d06e97f6232cfa9b248cb4c6fe40e098e6bff4c0
Robin Hübner <profan@pagefault.se>
2015-05-31 01:18:27 +0200

    Use Truffles exception for justice.

--- src/org/valarin/nodes/controlflow/ValReturnException.java
+++ src/org/valarin/nodes/controlflow/ValReturnException.java
@@ -1,9 +1,11 @@
 package org.valarin.nodes.controlflow;
 
+import com.oracle.truffle.api.nodes.ControlFlowException;
+
 /**
  * Created by RobinH on 2015-05-30.
  */
-public class ValReturnException extends Exception {
+public class ValReturnException extends ControlFlowException {
 
     public Object result;
 
