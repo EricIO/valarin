cb216c50275f2f5094b6fd1d989c8ae52743991c
Robin Hübner <profan@pagefault.se>
2015-06-02 09:17:05 +0200

    Fixed internal conflict.

--- src/org/valarin/nodes/ValAssignNode.java
+++ src/org/valarin/nodes/ValAssignNode.java
@@ -8,35 +8,35 @@ import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.FrameSlotKind;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
-/*
+
 @NodeChild("expression")
-@NodeField(name = "frame", type = FrameSlot.class)
+@NodeField(name = "frameSlot", type = FrameSlot.class)
 public abstract class ValAssignNode extends ValExpressionNode {
     
-    protected abstract FrameSlot getFrame();
+    protected abstract FrameSlot getFrameSlot();
     protected abstract Node getExpression();
 
     @Specialization(guards = "isLong()")
     protected long writeLong(VirtualFrame frame, long value) {
-        frame.setLong(this.getFrame(), value);
+        frame.setLong(this.getFrameSlot(), value);
         return value;
     }
     
     @Specialization(guards = "isBoolean()")
     protected boolean writeBoolean(VirtualFrame frame, boolean value) {
-        frame.setBoolean(this.getFrame(), value);
+        frame.setBoolean(this.getFrameSlot(), value);
         return value;
     }
     
     @Specialization(guards = "isDouble()")
     protected Double writeDouble(VirtualFrame frame, double value) {
-        frame.setDouble(this.getFrame(), value);
+        frame.setDouble(this.getFrameSlot(), value);
         return value;
     }
     
     @Specialization(contains = {"writeLong", "writeBoolean", "writeDouble"})
     protected Object write(VirtualFrame frame, Object value) {
-        FrameSlot slot = this.getFrame();
+        FrameSlot slot = this.getFrameSlot();
         if (slot.getKind() != FrameSlotKind.Object) {
             CompilerDirectives.transferToInterpreterAndInvalidate();
             slot.setKind(FrameSlotKind.Object);
@@ -47,15 +47,15 @@ public abstract class ValAssignNode extends ValExpressionNode {
     }
 
     protected boolean isLong() {
-        return this.isKind(this.getFrame(), FrameSlotKind.Long);
+        return this.isKind(this.getFrameSlot(), FrameSlotKind.Long);
     }
     
     protected boolean isBoolean() {
-        return this.isKind(this.getFrame(), FrameSlotKind.Boolean);
+        return this.isKind(this.getFrameSlot(), FrameSlotKind.Boolean);
     }
     
     protected boolean isDouble() {
-        return this.isKind(this.getFrame(), FrameSlotKind.Double);
+        return this.isKind(this.getFrameSlot(), FrameSlotKind.Double);
     }
     
     protected boolean isKind(FrameSlot frame, FrameSlotKind kind) {
@@ -70,4 +70,3 @@ public abstract class ValAssignNode extends ValExpressionNode {
         return false;
     }
 } 
-*/
